c7a1f968459a797163590f64a55799f05405c4a6
Narguss <narguss_w@pop-os.localdomain>
2021-10-18 18:03:03 +0200

    Ajout de la couleur des emojis et des calculs points

--- main.py
+++ main.py
@@ -38,10 +38,50 @@ def check_sort(stack):
 		i += 1;
 	return (1);
 
-def	check_data(stack_a, stack_b, n_testcase):
+def	check_number_of_instruction_points(size_of_stack, nb_of_cmd):
+	if (size_of_stack == 3):
+		if (nb_of_cmd == 2 or nb_of_cmd == 3):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅"+'\033[0m');
+		else :
+			print("SORT WITH TO MANY INSTRUCTIONS ==> " +  '\033[91m'+str(nb_of_cmd)+" instructions ❌" +'\033[0m');
+	elif (size_of_stack > 3 and size_of_stack <= 5):
+		if (nb_of_cmd <= 12):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅"+'\033[0m');
+		else :
+			print("SORT WITH TO MANY INSTRUCTIONS ==> " + '\033[91m'+str(nb_of_cmd)+ " instructions ❌"+'\033[0m');
+	elif (size_of_stack == 100):
+		if (nb_of_cmd < 700):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 5 POINTS"+'\033[0m');
+		elif (nb_of_cmd < 900):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 4 POINTS"+'\033[0m');
+		elif (nb_of_cmd < 1100):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 3 POINTS"+'\033[0m');
+		elif (nb_of_cmd < 1300):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 2 POINTS"+'\033[0m');
+		elif (nb_of_cmd < 1500):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 1 POINTS"+'\033[0m');
+		else:
+			print("SORT WITH TO MANY INSTRUCTIONS ==> " + '\033[91m'+ str(nb_of_cmd)+" instructions ❌"+'\033[0m');
+	elif (size_of_stack == 500):
+		if (nb_of_cmd < 5500):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 5 POINTS"+'\033[0m');
+		elif (nb_of_cmd < 7000):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 4 POINTS"+'\033[0m');
+		elif (nb_of_cmd < 8500):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 3 POINTS"+'\033[0m');
+		elif (nb_of_cmd < 10000):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 2 POINTS"+'\033[0m');
+		elif (nb_of_cmd < 11500):
+			print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅ 1 POINTS"+'\033[0m');
+		else:
+			print("SORT WITH TO MANY INSTRUCTIONS ==> " + '\033[91m' + str(nb_of_cmd)+" instructions ❌"+'\033[0m');
+	else :
+		print("SORT ==> "+ '\033[92m' +str(nb_of_cmd)+ " instructions ✅"+'\033[0m');
+
+def	check_data(stack_a, stack_b, n_testcase, size_of_stack):
 	command_file = open("command_test_"+ str(n_testcase) +".txt", "r");
 	command_file = command_file.readlines();
-	print("All is sort with " + str(len(command_file)) +" moves with :");
+	check_number_of_instruction_points(size_of_stack, len(command_file));
 	print("pa : ", str(command_file.count("pa\n")));
 	print("pb : ", str(command_file.count("pb\n")));
 	print("ra : ", str(command_file.count("ra\n")));
@@ -54,15 +94,15 @@ def	check_data(stack_a, stack_b, n_testcase):
 	print("sb : ", str(command_file.count("sb\n")));
 	print("ss : ", str(command_file.count("ss\n")));
 
-def all_is_good(stack_a, stack_b, n_testcase):
+def all_is_good(stack_a, stack_b, n_testcase, size_of_stack):
 	if not stack_b and check_sort(stack_a) == 1:
-		check_data(stack_a, stack_b, n_testcase);
+		check_data(stack_a, stack_b, n_testcase, size_of_stack);
 	else :
 		save_error_stack = open("stacks_of_test_"+ str(n_testcase)+".txt", "a");
 		if (check_sort(stack_a) == 0):
-			print("Stack not sort.");
+			print('\033[91m'+"NOT WORKS ❌ STACK_A IS NOT SORT ❌"+'\033[0m');
 		if stack_b:
-			print("Stack_b is not empty.");
+			print('\033[91m' + "NOT WORKS ❌ STACK_B IS NOT EMPTY ❌"+'\033[0m');
 		print_my_stacks(stack_a,stack_b, save_error_stack);
 		save_error_stack.close();
 
@@ -71,15 +111,16 @@ def	get_test(size_of_stack, n_testcase):
 	stack_b = [];
 	make_my_push_swap(stack_a, n_testcase);
 	if (read_and_exec_command(stack_a, stack_b, n_testcase) == 1):
-		all_is_good(stack_a, stack_b, n_testcase);
+		all_is_good(stack_a, stack_b, n_testcase, size_of_stack);
 		os.system("rm command_test_" + str(n_testcase) + ".txt");
 	else:
-		print("You display something that is not a command");
+		print('\033[91m' + "NOT WORKS ❌ You display something that is not a command ❌"+'\033[0m');
 
-number_of_test = int(sys.argv[1]);
-size_of_stack = int(sys.argv[2]);
-i = 1;
-while i <= number_of_test:
-	print("========================= TEST_CASE N°",str(i), " =========================");
-	get_test(size_of_stack, i);
-	i += 1;
+if __name__ == "__main__":
+	number_of_test = int(sys.argv[1]);
+	size_of_stack = int(sys.argv[2]);
+	i = 1;
+	while i <= number_of_test:
+		print('\033[93m'+"========================= TEST_CASE N°",str(i), " ========================="+'\033[0m');
+		get_test(size_of_stack, i);
+		i += 1;
